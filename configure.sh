152976ece632ab7fd3f8840e63642dab36467c94
shao.jiacheng <shao.jiacheng@99cloud.net>
2021-05-28 00:37:27 +0800

    优化掉了nginx的配置

--- configure.sh
+++ configure.sh
@@ -6,18 +6,12 @@ export mysql_root_password=123456
 export task_log_file_path=/mnt/jianmu/ci/log
 # 存放平台任务定义目录
 export task_def_file_path=/mnt/jianmu/ci/task_def
-# nginx配置文件路径
-export nginx_conf_file_path=/home/nginx.conf
-# nginx ssl证书文件路径
-export nginx_ssl_cert_dir=/home/cert
 # 后台服务tag
 export server_tag=0.2
 # 前端tag
 export web_tag=0.2
 # 平台admin用户密码
 export jianmu_password=123456
-# nginx server_name
-export nginx_server_name=172.20.150.125
 #------------------------------------------------------------------------------
 
 function ergodic(){
@@ -27,7 +21,7 @@ function ergodic(){
         then
              ergodic $1"/"$file
         else
-             envsubst '${nginx_server_name},${mysql_root_password},${task_log_file_path},${task_def_file_path},${nginx_conf_file_path},${nginx_ssl_cert_dir},${server_tag},${web_tag},${jianmu_password}' < $1"/"$file > $1"/"${file%.*}
+             envsubst '${mysql_root_password},${task_log_file_path},${task_def_file_path},${server_tag},${web_tag},${jianmu_password}' < $1"/"$file > $1"/"${file%.*}
              rm -f $1"/"$file
         fi
     done
